3bea3ce2eed3f6d975da2ba46cdd003206d6c7a3
rporter03 <porterclayton2003@gmail.com>
2025-09-23 14:10:14 -0400

    javascript loops

---
+++ csce242/examples/04_js/js/loops.js
@@ -0,0 +1,25 @@
+document.getElementById("btn-display").onclick = () => {
+    const displaySection = document.getElementById("loop-result");
+    const ul = document.createElement("ul");
+    displaySection.append(ul);
+
+    // loops - write numbers 1-10
+    for(let i = 0; i < 10; i++) {
+        const li = document.createElement("li");
+        li.innerHTML = `${i} banana(s)`;
+        ul.append(li);
+    }
+};
+
+document.getElementById("btn-count").onclick = () => {
+    const error = document.getElementById("error-number");
+    error.innerHTML = "";
+    const startNum = parseInt(document.getElementById("txt-start").value);
+    const endNum = parseInt(document.getElementById("txt-end").value);
+    const resultDiv = document.getElementById("d-count");
+    resultDiv.innerHTML = "";
+    if (startNum > endNum) {
+        error.innerHTML = "* The second number must be bigger than the first";
+        return;
+    } 
+};
